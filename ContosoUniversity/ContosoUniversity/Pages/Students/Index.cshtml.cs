65824310d3de3ecb9a20ae8cdfb044a9eb1214a6
Jun-hee Cho <lcscho22@gmail.com>
2024-03-15 11:29:11 +0900

    commit

---
+++ ContosoUniversity/ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
