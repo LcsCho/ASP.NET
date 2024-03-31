381f3edd2fc8c388e15eb9c6800f0c6581ccbad7
Jun-hee Cho <chojunhee98@gmail.com>
2024-03-31 21:19:12 +0900

    Hello World!

---
+++ ConsoleApp1/Hello_World.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hello_World
+    {
+        static void Main()
+        {
+            AA var = new AA();
+            var.PrintHello("Hello World!");
+        }
+    }
+
+    class AA
+    {
+        public void PrintHello(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
